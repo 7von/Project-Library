a306a966962f294cceabbaa453f17b9fb746b5d9
7von <cevon.sieh@zohomail.com>
2021-05-02 23:31:49 -0400

    Created skeleton files

---
+++ index.js
@@ -0,0 +1,17 @@
+let myLibrary = []
+
+function Book(name, author, pages){
+    this.name
+    this.author
+    this.pages
+}
+
+book = new Book()
+
+function addBookToLibrary(book){
+    if (book == myLibrary.map(book)){
+        myLibrary.push(book)
+    }
+}
+
+
