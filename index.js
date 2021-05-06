c1b721aedd3557f550603db429f6d0a7538b4ccf
7von <cevon.sieh@zohomail.com>
2021-05-05 21:02:13 -0400

    Debuging addBookToLibrary function

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 let myLibrary = []
 
 function Book(name, author, pages){
-    this.name
-    this.author
-    this.pages
+    this.name = name
+    this.author = author
+    this.pages = pages
 }
 
-book = new Book()
+Book.prototype.hasRead = function(){
+    this.hasRead = prompt("Have you read this book yet? Y/N")
+}
 
-function addBookToLibrary(book){
-    if (book == myLibrary.map(book)){
-        myLibrary.push(book)
+function addBookToLibrary(obj){
+    if(myLibrary >= 0){
+        checkLibrary(obj)
     }
+    else {return myLibrary.push(obj)}
 }
+
+function checkLibrary(obj){
+    if(myLibrary.every(e => e === obj)){
+        console.log("testing")
+        return myLibrary.push(obj)
+    }
+    return null
+}
+
+
+const book = new Book("Harry Potter", "J.K. Rowlings", 245)
+const book2 = new Book("The Hobbit", "Tolkien", 340)
+
+addBookToLibrary(book)
+addBookToLibrary(book2)
+
+console.table(myLibrary)
 
 
